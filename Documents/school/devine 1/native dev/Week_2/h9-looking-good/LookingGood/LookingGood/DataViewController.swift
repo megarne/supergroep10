22993daf59d3e1086b7c1f09c780ae443bb862e9
megarne <cavia@live.be>
2015-05-27 10:30:07 +0200

    first commit

---
+++ Documents/school/devine 1/native dev/Week_2/h9-looking-good/LookingGood/LookingGood/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  LookingGood
+//
+//  Created by arne de-cavia-prez on 2/19/15.
+//  Copyright (c) 2015 arne deprez. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 supergroep10
 ============
+# BADGEThumo
